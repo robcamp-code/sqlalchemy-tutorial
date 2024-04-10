1588f748541cf9579d6e6a9a49b1185576eb9ed9
Robert Campbell <robcampbell26042604@gmail.com>
2024-04-10 07:56:55 -0400

    added league to schema

--- src/models/schema.py
+++ src/models/schema.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, Date
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, Date
 from sqlalchemy.orm import Relationship
 from .base import Model
 from .base import TimeStampedModel
@@ -8,13 +8,16 @@ class Fixture(TimeStampedModel):
     __tablename__ = "fixture"
     id = Column(Integer, primary_key=True, autoincrement=True)
     
+    league_id = Column(Integer, ForeignKey("league.id", ondelete="CASCADE"), nullable=False, index=True)
     home_team_id = Column(Integer, ForeignKey("team.id", ondelete="CASCADE"), nullable=False, index=True)
     away_team_id = Column(Integer, ForeignKey("team.id", ondelete="CASCADE"), nullable=False, index=True)
+
     home_goals = Column(Integer, nullable=True)
     away_goals = Column(Integer, nullable=True)
     season = Column(Integer, nullable=False)
     start_time = Column(Date, nullable=False)
 
+    league = Relationship("League", back_populates="fixture", passive_deletes=True)
     home_team = Relationship("Team", back_populates="fixture", passive_deletes=True)
     away_team = Relationship("Team", back_populates="fixture", passive_deletes=True)
     game_info = Relationship("GameInfo", back_populates="fixture", passive_deletes=True)
@@ -29,6 +32,16 @@ class Team(Model):
     away_team = Relationship("Fixture", back_populates="team")
 
     players = Relationship("player")
+
+
+class League(Model):
+    __tablename__ = "league"
+    
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, nullable=False)
+    is_cup = Column(Boolean, nullable=False, default=False)
+
+    fixture = Relationship("Fixture", back_populates="league", passive_deletes=True)
 
 
 class TeamPlayer(Model):
